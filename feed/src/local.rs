7b3c1d6267f0a0a5b66de69ed59685012c19ce5d
Ruihang Xia <waynestxia@gmail.com>
2023-04-06 22:54:46 +0800

    feat: get change file list
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- feed/src/local.rs
+++ feed/src/local.rs
@@ -1,7 +1,8 @@
 use std::str::FromStr;
 
+use gix_diff::tree::Changes;
 use gix_hash::ObjectId;
-use gix_odb::{FindExt, Handle};
+use gix_odb::{Find, FindExt, Handle};
 use gix_traverse::commit::{ancestors, Ancestors};
 use snafu::{OptionExt, ResultExt};
 
@@ -43,22 +44,15 @@ impl FetchTask {
     }
 
     pub fn execute(&self, consumer: &dyn Consumer) -> FeedResult<()> {
-        let head = self
-            .find_head()
-            .map_err(error::boxed)
-            .context(GeneralSnafu)?;
+        let all_commits = self.find_commits(Some(self.since))?;
 
-        let all_commits = Ancestors::new(Some(head), ancestors::State::default(), |oid, buf| {
-            self.db.find_commit_iter(oid, buf)
-        })
-        .collect::<Result<Vec<_>, gix_traverse::commit::ancestors::Error>>()
-        .map_err(error::boxed)
-        .context(GeneralSnafu)?;
+        for window in all_commits.as_slice().windows(2) {
+            self.process_diff(window[0], window[1]).unwrap();
+        }
 
         let mut buf = Vec::new();
         for oid in &all_commits {
             let commit = self.db.find_commit(oid, &mut buf).unwrap();
-            // println!("commit: {:?}", commit);
             let record = Record {
                 commit_time: format!("{}", commit.time().seconds_since_unix_epoch),
                 author_name: commit.author().name.to_string(),
@@ -70,7 +64,6 @@ impl FetchTask {
                 calc_time: String::new(),
             };
             consumer.record(record);
-            // println!("{record:#?}")
         }
 
         Ok(())
@@ -90,4 +83,90 @@ impl FetchTask {
         let buf = std::fs::read(path).context(FileSystemSnafu)?;
         ObjectId::from_hex(&buf.trim_ascii_end()).context(ConvertObjectIdSnafu)
     }
+
+    /// Find all commits before `until`. `until` is included
+    fn find_commits(&self, until: Option<ObjectId>) -> FeedResult<Vec<ObjectId>> {
+        let head = self
+            .find_head()
+            .map_err(error::boxed)
+            .context(GeneralSnafu)?;
+
+        let all_commits = Ancestors::new(Some(head), ancestors::State::default(), |oid, buf| {
+            self.db.find_commit_iter(oid, buf)
+        })
+        .collect::<Result<Vec<_>, gix_traverse::commit::ancestors::Error>>()
+        .map_err(error::boxed)
+        .context(GeneralSnafu)?;
+
+        // todo: improve this
+        let filtered_commits = if let Some(until) = until {
+            let mut result = Vec::new();
+            for oid in all_commits {
+                result.push(oid);
+                if oid == until {
+                    break;
+                }
+            }
+            result
+        } else {
+            all_commits
+        };
+
+        Ok(filtered_commits)
+    }
+
+    fn process_diff(&self, before: ObjectId, after: ObjectId) -> FeedResult<()> {
+        let mut before_buf = Vec::new();
+        let mut after_buf = Vec::new();
+
+        let before_commit = self
+            .db
+            .find_commit(&before, &mut before_buf)
+            .unwrap()
+            .tree();
+        let after_commit = self.db.find_commit(&after, &mut after_buf).unwrap().tree();
+
+        let before_tree = self
+            .db
+            .find_tree_iter(before_commit, &mut before_buf)
+            .unwrap();
+        let after_tree = self
+            .db
+            .find_tree_iter(after_commit, &mut after_buf)
+            .unwrap();
+
+        let mut recorder = gix_diff::tree::Recorder::default();
+        Changes::from(before_tree)
+            .needed_to_obtain(
+                after_tree,
+                &mut gix_diff::tree::State::default(),
+                |oid, buf| {
+                    // use gix_odb::pack::FindExt;
+                    self.db.find_tree_iter(oid, buf)
+                    // .map(|(obj, _)| obj.try_into_tree_iter().expect("only called for trees"))
+                },
+                &mut recorder,
+            )
+            .unwrap();
+
+        println!("from {} to {}:", before, after);
+        println!("{:#?}", recorder);
+
+        // let before_tree = before_commit.tree().unwrap();
+        // let after_tree = after_commit.tree().unwrap();
+
+        // let diff = gix_diff::diff_tree(&self.db, &before_tree, &after_tree).unwrap();
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use snafu::ErrorCompat;
+
+    use super::*;
+    use crate::consumer::PrintConsumer;
 }
--- feed/src/schema.rs
+++ feed/src/schema.rs
@@ -17,3 +17,39 @@ pub enum Operation {
     Add,
     Remove,
 }
+
+pub struct RecordBuilder {
+    pub commit_time: String,
+    pub author_name: String,
+    pub author_email: String,
+    pub commit_message: String,
+}
+
+impl RecordBuilder {
+    pub fn new_base(
+        commit_time: String,
+        author_name: String,
+        author_email: String,
+        commit_message: String,
+    ) -> Self {
+        Self {
+            commit_time,
+            author_name,
+            author_email,
+            commit_message,
+        }
+    }
+
+    pub fn build(&self, operation: Operation, file_path: String, content: String) -> Record {
+        Record {
+            commit_time: self.commit_time.clone(),
+            author_name: self.author_name.clone(),
+            author_email: self.author_email.clone(),
+            operation,
+            file_path,
+            commit_messaage: self.commit_message.clone(),
+            content,
+            calc_time: String::new(),
+        }
+    }
+}
