c7de7b4b499972884596e3962ec3880938ae929c
Ruihang Xia <waynestxia@gmail.com>
2023-05-01 22:27:44 +0800

    feat: rough skeleton of the page
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ page/uno.config.js
@@ -0,0 +1,9 @@
+import { defineConfig, presetAttributify, presetUno, presetIcons } from 'unocss';
+
+export default defineConfig({
+    presets: [
+        presetAttributify({ /* preset options */ }),
+        presetUno(),
+        presetIcons(),
+    ],
+});
